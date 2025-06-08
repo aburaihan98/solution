bd83f15f52bcf42b4be59011095657988777e657
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-08 09:23:44 -0700

    Complete solution 2

--- src/solution.ts
+++ src/solution.ts
@@ -12,3 +12,17 @@ function formatString(input: string, toUpper?: boolean): string {
 // formatString("Hello", true);
 // formatString("Hello", false);
 // console.log(formatString("Hello"));
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item?.rating >= 4);
+}
+
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book A", rating: 4 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 5.0 },
+// ];
+// console.log(filterByRating(books));
