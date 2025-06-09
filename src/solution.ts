b08529830d57e268fd0f4325ef82077e3ac62576
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-08 21:27:05 -0700

    Complete solution 8

--- src/solution.ts
+++ src/solution.ts
@@ -129,3 +129,18 @@ function getDayType(day: Day): string {
 
 // console.log(getDayType(Day.Monday)); // Output: "Weekday"
 // console.log(getDayType(Day.Sunday)); // Output: "Weekend"
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((res, rej) => {
+    if (n >= 0) {
+      setTimeout(() => {
+        res(n * n);
+      }, 1000);
+    } else {
+      rej(new Error("Negative number not allowed"));
+    }
+  });
+}
+
+// squareAsync(4).then((result) => console.log("Square :", result)); // Output after 1s: 16
+// squareAsync(-3).catch((result) => console.log(result)); // Output: Error: Negative number not allowed
